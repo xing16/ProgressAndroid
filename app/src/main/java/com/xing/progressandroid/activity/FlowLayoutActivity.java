dacd929352f3d4218f64f45cb3f573012badc1b8
xing <1429161712@qq.com>
2019-04-01 22:08:41 +0800

    FlowLayout2 增加效果图

--- app/src/main/java/com/xing/progressandroid/activity/FlowLayoutActivity.java
+++ app/src/main/java/com/xing/progressandroid/activity/FlowLayoutActivity.java
@@ -2,6 +2,7 @@ package com.xing.progressandroid.activity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -9,19 +10,22 @@ import android.widget.TextView;
 import com.xing.progressandroid.R;
 import com.xing.progressandroid.base.BaseActivity;
 import com.xing.progressandroid.customview.flowlayout.FlowAdapter;
-import com.xing.progressandroid.customview.flowlayout.FlowLayout;
 import com.xing.progressandroid.customview.flowlayout.FlowLayout2;
+
+import java.util.Random;
 
 public class FlowLayoutActivity extends BaseActivity {
 
     private String[] data = {"Android", "Java", "Python", "linux", "iOS", "SpringBoot2.x",
             "MacBookPro", "windows", "RxJava2.x", "插件化组件化", "反射，动态代理"};
+    private int[] heights;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flow_layout);
-        FlowLayout flowLayout = findViewById(R.id.fl_flow);
+        heights = new int[]{dp2Px(20), dp2Px(30), dp2Px(40), dp2Px(25), dp2Px(60), dp2Px(50)};
+        FlowLayout2 flowLayout = findViewById(R.id.fl_flow);
         FlowLayout2 flowLayout2 = findViewById(R.id.fl_flow2);
         flowLayout.setAdapter(new FlowAdapter() {
             @Override
@@ -48,8 +52,10 @@ public class FlowLayoutActivity extends BaseActivity {
             public View getView(int position, ViewGroup parent) {
                 TextView textView = new TextView(FlowLayoutActivity.this);
                 textView.setText(data[position]);
+                textView.setBackgroundResource(R.drawable.shape_flow2_item_bg);
                 textView.setTextColor(Color.parseColor("#ff555555"));
-                textView.setBackgroundResource(R.drawable.shape_flow_item_bg);
+                ViewGroup.MarginLayoutParams lp = new ViewGroup.MarginLayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, heights[new Random().nextInt(heights.length)]);
+                textView.setLayoutParams(lp);
                 if (position == 4) {
                     textView.setVisibility(View.GONE);
                 }
@@ -57,4 +63,10 @@ public class FlowLayoutActivity extends BaseActivity {
             }
         });
     }
+
+
+    private int dp2Px(int dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, getResources().getDisplayMetrics());
+    }
+
 }
